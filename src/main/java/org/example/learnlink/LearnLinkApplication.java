bf9e1dbb793aed0109130e9a9d851eff4223fdd8
zakariae elmoufid <moufidzakarie3@gmail.com>
2026-01-28 14:38:27 +0100

    first commit

---
+++ src/main/java/org/example/learnlink/LearnLinkApplication.java
@@ -0,0 +1,13 @@
+package org.example.learnlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLinkApplication.class, args);
+    }
+
+}
